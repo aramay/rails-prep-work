2855405502841529e0b3a7263cf6701f2d19d601
Abid Ramay <kasper_341@hotmail.com>
2016-07-06 11:48:08 -0700

    Add show method method to display single article

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,7 +1,26 @@
 class ArticlesController < ApplicationController
+  # standard CRUD actions in each controller in the following order: index, show, new, edit, create, update and destroy.
+  def show
+    @article = Article.find(params[:id])
+  end
 
   def new
 
+  end
+
+  def create
+    # render plain: params[:article].inspect
+    # @article = Article.new(params[:article])
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
+
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 
 end
