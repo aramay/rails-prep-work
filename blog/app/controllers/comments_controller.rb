6045edb30b475e0628cacda0f5c440014e267bb6
Abid Ramay <kasper_341@hotmail.com>
2016-07-06 16:11:31 -0700

    Add comment#destroy method

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -8,6 +8,15 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+
+    @comment.destroy
+    redirect_to article_path(@article)
+
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:commenter, :body)
