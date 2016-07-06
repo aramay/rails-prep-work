e844cdd0024b12f970fbe65cd07c4a94b076c8d1
Abid Ramay <kasper_341@hotmail.com>
2016-07-06 12:00:17 -0700

    Add index view to list all articles

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
   # standard CRUD actions in each controller in the following order: index, show, new, edit, create, update and destroy.
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end
