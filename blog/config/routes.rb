e2a0b0ccfad42dc93e8fd97d2f9adc9ec18be784
Abid Ramay <kasper_341@hotmail.com>
2016-07-06 10:48:53 -0700

    Update article resource

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  #Add article resources
+  resources :articles
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
