adca9aadb41e2a7799dd32e943504b07372e00c0
Abid Ramay <kasper_341@hotmail.com>
2016-07-07 11:24:46 -0700

    reinitialize

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,58 @@
+class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  # standard CRUD actions in each controller in the following order: index, show, new, edit, create, update and destroy.
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    # binding.pry
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    # render plain: params[:article].inspect
+    # @article = Article.new(params[:article])
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+
+  end
+
+  def destroy
+
+    @article = Article.find(params[:id]).destroy
+
+    redirect_to articles_path
+
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+end
