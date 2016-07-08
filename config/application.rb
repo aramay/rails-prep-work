dfef59392bec0640c205c82c902f729b20c08fed
Abid Ramay <kasper_341@hotmail.com>
2016-07-08 12:58:46 -0700

    Add formating

--- config/application.rb
+++ config/application.rb
@@ -33,14 +33,14 @@ module Blog
     config.active_record.raise_in_transactional_callbacks = true
 
     config.generators do |g|
-  g.test_framework :rspec,
-    :fixtures => true,
-    :view_specs => false,
-    :helper_specs => false,
-    :routing_specs => false,
-    :controller_specs => true,
-    :request_specs => true
-  g.fixture_replacement :factory_girl, :dir => "spec/factories"
-end
+      g.test_framework :rspec,
+      :fixtures => true,
+      :view_specs => false,
+      :helper_specs => false,
+      :routing_specs => false,
+      :controller_specs => true,
+      :request_specs => true
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
   end
+end
 end
