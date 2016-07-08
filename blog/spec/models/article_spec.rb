a002661e71c063fb704991c9c6536e62a4d60a87
Abid Ramay <kasper_341@hotmail.com>
2016-07-07 17:55:37 -0700

    Add specs

---
+++ blog/spec/models/article_spec.rb
@@ -0,0 +1,13 @@
+
+
+# spec/models/contact.rb
+require 'spec_helper'
+
+describe Article do
+  it 'has a valid factory' do
+    FactoryGirl.create(:article).should be_valid
+  end
+  it "has a title"
+
+  it "has text"
+end
